1e68160711f9be109deeee666d6d054db1cfc0ac
alexandre lusa <jiresselusa127@gmail.com>
2025-09-22 20:46:26 -0700

    Update data.mjs

--- js/data.mjs
+++ js/data.mjs
@@ -1,5 +1,8 @@
 export async function getAbonnements() {
-  const response = await fetch("../data/abonnements.json");
+  const response = await fetch("/application-web/data/abonnements.json");
+  // ou simplement "data/abonnements.json" si ton site est servi depuis /application-web/
+  // selon comment GitHub Pages est configuré
+
   if (!response.ok) {
     throw new Error("Impossible de charger le JSON");
   }
